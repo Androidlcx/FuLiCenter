13c4aafb56d96dc856b3f5f2a7b913fd5d6072eb
Androidlcx <3091613162@qq.com>
2016-10-14 15:44:07 +0800

    添加log输出的工具类

--- app/src/main/java/cn/ucai/fulicenter/utils/L.java
+++ app/src/main/java/cn/ucai/fulicenter/utils/L.java
@@ -6,6 +6,7 @@ import android.util.Log;
  * Created by clawpo on 16/9/20.
  */
 public class L {
+    //logcat开关
     public static boolean isDebug = true;
     private static final String TAG = "fulicenter";
 
