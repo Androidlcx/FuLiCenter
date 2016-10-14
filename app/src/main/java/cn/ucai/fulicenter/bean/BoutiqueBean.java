aafe56bda5aff539d90e820123f37b7a3a1bfecb
Androidlcx <3091613162@qq.com>
2016-10-14 11:25:34 +0800

    amend bean  BoutiqueBean2

--- app/src/main/java/cn/ucai/fulicenter/bean/BoutiqueBean.java
+++ app/src/main/java/cn/ucai/fulicenter/bean/BoutiqueBean.java
@@ -4,15 +4,6 @@ package cn.ucai.fulicenter.bean;
  * Created by Administrator on 2016/10/13.
  */
 public class BoutiqueBean {
-
-    /**
-     * id : 262
-     * title : 不一样的新妆，不一样的美丽
-     * description : 快速增长修护预防脱发洗发水让头发健康快速生长更美丽
-     * name : 拯救头发，美丽新妆
-     * imageurl : cat_image/boutique1.png
-     */
-
     private int id;
     private String title;
     private String description;
