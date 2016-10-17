299022379fe70155593065bf3ef8ca44e5a29f7f
Androidlcx <3091613162@qq.com>
2016-10-17 10:49:10 +0800

    butterknife的添加，布局文件RadioGroup的删除

--- app/src/main/java/cn/ucai/fulicenter/acitivity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/acitivity/MainActivity.java
@@ -3,19 +3,79 @@ package cn.ucai.fulicenter.acitivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.utils.L;
 
 public class MainActivity extends AppCompatActivity {
 
+    @Bind(R.id.image)
+    ImageView image;
+    @Bind(R.id.xp)
+    RadioButton xp;
+    @Bind(R.id.jx)
+    RadioButton jx;
+    @Bind(R.id.fl)
+    RadioButton fl;
+    @Bind(R.id.tvcart)
+    TextView tvcart;
+    @Bind(R.id.gwc)
+    RadioButton gwc;
+    @Bind(R.id.me)
+    RadioButton me;
+
+    int index;
+    RadioButton [] rbs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        L.i("MainActivity onCreate");
+        ButterKnife.bind(this);
+        initView();
     }
-    public void oncCheckedChange(View view){
 
+    private void initView() {
+        rbs = new RadioButton [5];
+        rbs[0] = xp;
+        rbs[1] = jx;
+        rbs[2] = fl;
+        rbs[3] = gwc;
+        rbs[4] = me;
+    }
+
+    public void onCheckedChange(View view) {
+        switch (view.getId()) {
+            case R.id.xp:
+                index = 0;
+                break;
+            case R.id.jx:
+                index = 1;
+                break;
+            case R.id.fl:
+                index = 2;
+                break;
+            case R.id.gwc:
+                index = 3;
+                break;
+            case R.id.me:
+                index = 4;
+                break;
+        }
+        setRadioButtonStatus();
+    }
+
+    private void setRadioButtonStatus() {
+        for (int i = 0;i<rbs.length;i++){
+            if (i == index){
+                rbs[i].setChecked(true);
+            }else{
+                rbs[i].setChecked(false);
+            }
+        }
     }
 }
