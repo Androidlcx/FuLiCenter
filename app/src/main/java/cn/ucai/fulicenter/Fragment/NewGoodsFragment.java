243ee03cf2ba57f4934ee985c9fef4f7e7ff3bff
Androidlcx <3091613162@qq.com>
2016-10-17 20:48:57 +0800

    添加NewGoodsFragment

--- app/src/main/java/cn/ucai/fulicenter/Fragment/NewGoodsFragment.java
+++ app/src/main/java/cn/ucai/fulicenter/Fragment/NewGoodsFragment.java
@@ -1,10 +1,93 @@
-package cn.ucai.fulicenter.Fragment;
+package cn.ucai.fulicenter.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import cn.ucai.fulicenter.I;
+import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.acitivity.MainActivity;
+import cn.ucai.fulicenter.adapter.GoodsAdapter;
+import cn.ucai.fulicenter.bean.NewGoodsBean;
+import cn.ucai.fulicenter.net.NetDao;
+import cn.ucai.fulicenter.net.OkHttpUtils;
+import cn.ucai.fulicenter.utils.ConvertUtils;
+import cn.ucai.fulicenter.utils.L;
 
 /**
  * Created by Administrator on 2016/10/17.
  */
 public class NewGoodsFragment extends Fragment {
+    @Bind(R.id.tv_refresh)
+    TextView tvRefresh;
+    @Bind(R.id.rv)
+    RecyclerView rv;
+    @Bind(R.id.srl)
+    SwipeRefreshLayout srl;
 
+    MainActivity mContext;
+    GoodsAdapter mAdapter;
+    ArrayList<NewGoodsBean>mList;
+
+    int pageId = 1;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.fragment_newgoods, container, false);
+        ButterKnife.bind(this, layout);
+        mContext = (MainActivity) getContext();
+        mList = new ArrayList<>();
+        mAdapter = new GoodsAdapter(mContext,mList);
+        initView();
+        initData();
+        return layout;
+    }
+
+    private void initData() {
+        //页面显示，网络请求
+        NetDao.downloadNewGoods(mContext, pageId, new OkHttpUtils.OnCompleteListener<NewGoodsBean[]>() {
+            @Override
+            public void onSuccess(NewGoodsBean[] result) {
+                if (result != null && result.length>0){
+                    ArrayList<NewGoodsBean> list = ConvertUtils.array2List(result);
+                    mAdapter.initData(list);
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+                L.e("error"+error);
+            }
+        });
+    }
+
+    private void initView() {
+        /*下拉刷新小圆圈的颜色*/
+        srl.setColorSchemeColors(
+                getResources().getColor(R.color.google_blue),
+                getResources().getColor(R.color.google_green),
+                getResources().getColor(R.color.google_red),
+                getResources().getColor(R.color.google_yellow)
+        );
+        GridLayoutManager glm = new GridLayoutManager(mContext, I.COLUM_NUM);
+        rv.setLayoutManager(glm);
+        rv.setHasFixedSize(true);//修复图片大小
+        rv.setAdapter(mAdapter);
+    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
 }
