e8841700ce6ae1d749d505a5989be2f217112d9a
Androidlcx <3091613162@qq.com>
2016-10-18 19:27:25 +0800

    添加两个自定义控件

---
+++ app/src/main/java/cn/ucai/fulicenter/acitivity/GoodsDetailActivity.java
@@ -0,0 +1,95 @@
+package cn.ucai.fulicenter.acitivity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import cn.ucai.fulicenter.I;
+import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.View.FlowIndicator;
+import cn.ucai.fulicenter.View.SlideAutoLoopView;
+import cn.ucai.fulicenter.bean.GoodsDetailsBean;
+import cn.ucai.fulicenter.net.NetDao;
+import cn.ucai.fulicenter.net.OkHttpUtils;
+import cn.ucai.fulicenter.utils.CommonUtils;
+import cn.ucai.fulicenter.utils.L;
+
+public class GoodsDetailActivity extends AppCompatActivity {
+
+    @Bind(R.id.backClickArea)
+    LinearLayout backClickArea;
+    @Bind(R.id.tv_good_name_english)
+    TextView tvGoodNameEnglish;
+    @Bind(R.id.tv_good_name)
+    TextView tvGoodName;
+    @Bind(R.id.tv_good_price_shop)
+    TextView tvGoodPriceShop;
+    @Bind(R.id.tv_good_price_current)
+    TextView tvGoodPriceCurrent;
+    @Bind(R.id.salv)
+    SlideAutoLoopView salv;
+    @Bind(R.id.indicator)
+    FlowIndicator indicator;
+    @Bind(R.id.wv_goods_brief)
+    WebView wvGoodsBrief;
+
+    int goodsId;
+    GoodsDetailActivity mContext;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goods_detail);
+        ButterKnife.bind(this);
+        goodsId = getIntent().getIntExtra(I.GoodsDetails.KEY_GOODS_ID, 0);
+        L.e("details", "goodsid=" + goodsId);
+        if (goodsId == 0){
+            finish();
+        }
+        mContext = this;
+        initView();
+        initData();
+        setListener();
+    }
+
+    private void setListener() {
+
+    }
+
+    private void initData() {
+        NetDao.downloadGoodsDetail(mContext, goodsId, new OkHttpUtils.OnCompleteListener<GoodsDetailsBean>() {
+            @Override
+            public void onSuccess(GoodsDetailsBean result) {
+                L.i("details="+result);
+                if (result != null){
+                    showGoodDetails(result);
+                }else {
+                    finish();
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+                finish();
+                L.e("details,error="+error);
+                CommonUtils.showLongToast(error);
+            }
+        });
+    }
+//数据填充
+    private void showGoodDetails(GoodsDetailsBean details) {
+        tvGoodNameEnglish.setText(details.getGoodsEnglishName());
+        tvGoodName.setText(details.getGoodsName());
+        tvGoodPriceCurrent.setText(details.getCurrencyPrice());
+        tvGoodPriceShop.setText(details.getShopPrice());
+        //轮播图片以及外部wieb
+
+    }
+
+    private void initView() {
+
+    }
+}
