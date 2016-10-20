2c5ffa692310ac7d47718bff302fb13fa9a87e7d
Androidlcx <3091613162@qq.com>
2016-10-20 19:20:41 +0800

    分类二级页面数据排序后修改箭头的方向

--- app/src/main/java/cn/ucai/fulicenter/acitivity/CategoryChildActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/acitivity/CategoryChildActivity.java
@@ -1,5 +1,6 @@
 package cn.ucai.fulicenter.acitivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.GridLayoutManager;
@@ -174,22 +175,32 @@ public class CategoryChildActivity extends BaseActivity {
 
     @OnClick({R.id.btn_sort_price, R.id.btn_sort_addtime})
     public void onClick(View view) {
+        Drawable right;
         L.e("sortby,,,");
         switch (view.getId()) {
             case R.id.btn_sort_price:
                 if (priceasc){
                     sortBY = I.SORT_BY_PRICE_ASC;
+                    //箭头方向
+                    right = getResources().getDrawable(R.mipmap.arrow_order_up);
                 }else {
                     sortBY = I.SORT_BY_PRICE_DESC;
+                    right = getResources().getDrawable(R.mipmap.arrow_order_down);
                 }
+                right.setBounds(0,0,right.getIntrinsicWidth(),right.getIntrinsicHeight());
+                btnSortPrice.setCompoundDrawablesRelativeWithIntrinsicBounds(null,null,right,null);
                 priceasc = !priceasc;
                 break;
             case R.id.btn_sort_addtime:
                 if (addTimeAsc){
                     sortBY = I.SORT_BY_ADDTIME_ASC;
+                    right = getResources().getDrawable(R.mipmap.arrow_order_up);
                 }else {
                     sortBY = I.SORT_BY_ADDTIME_DESC;
+                    right = getResources().getDrawable(R.mipmap.arrow_order_down);
                 }
+                right.setBounds(0,0,right.getIntrinsicWidth(),right.getIntrinsicHeight());
+                btnSortAddtime.setCompoundDrawablesRelativeWithIntrinsicBounds(null,null,right,null);
                 addTimeAsc = !addTimeAsc;
                 break;
         }
