be9aa589b5f682cf2b4aa9ad0fd01d20635cbd66
Androidlcx <3091613162@qq.com>
2016-10-25 20:31:55 +0800

    更新用户头像实现

--- app/src/main/java/cn/ucai/fulicenter/acitivity/UserProfileActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/acitivity/UserProfileActivity.java
@@ -1,10 +1,13 @@
 package cn.ucai.fulicenter.acitivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.File;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -13,11 +16,17 @@ import cn.ucai.fulicenter.FuLiCenterApplication;
 import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.View.DisplayUtils;
+import cn.ucai.fulicenter.bean.Result;
 import cn.ucai.fulicenter.bean.User;
 import cn.ucai.fulicenter.dao.SharePrefrenceUtils;
+import cn.ucai.fulicenter.net.NetDao;
+import cn.ucai.fulicenter.net.OkHttpUtils;
 import cn.ucai.fulicenter.utils.CommonUtils;
 import cn.ucai.fulicenter.utils.ImageLoader;
+import cn.ucai.fulicenter.utils.L;
 import cn.ucai.fulicenter.utils.MFGT;
+import cn.ucai.fulicenter.utils.OnSetAvatarListener;
+import cn.ucai.fulicenter.utils.ResultUtils;
 
 public class UserProfileActivity extends BaseActivity {
 
@@ -30,7 +39,7 @@ public class UserProfileActivity extends BaseActivity {
 
     UserProfileActivity mContext;
     User user = null;
-
+    OnSetAvatarListener mOnSetAvatarListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_user_profile);
@@ -63,6 +72,8 @@ public class UserProfileActivity extends BaseActivity {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.rl_UserNameAvatar:
+                mOnSetAvatarListener = new OnSetAvatarListener(mContext,R.id.layout_upload_avatar,
+                        user.getMuserName(),I.AVATAR_TYPE_USER_PATH);
                 break;
             case R.id.rl_UserName:
                 CommonUtils.showLongToast(R.string.user_name_connot_be_modify);
@@ -95,9 +106,54 @@ public class UserProfileActivity extends BaseActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RESULT_OK && requestCode == I.REQUEST_CODE_NICK){
+        L.e("onActivityResult,requestCode="+requestCode+",resultCode="+resultCode);
+        if (resultCode != RESULT_OK){
+            return;
+        }
+        mOnSetAvatarListener.setAvatar(requestCode,data,imPersonalDataUserAvatar);
+        if (requestCode == I.REQUEST_CODE_NICK){
             CommonUtils.showLongToast(R.string.update_user_nick_success);
         }
+        if (requestCode == OnSetAvatarListener.REQUEST_CROP_PHOTO){
+                updateAvatar();
+        }
+    }
+//更新图片的方法
+    private void updateAvatar() {
+        File file = new File(OnSetAvatarListener.getAvatarPath(mContext,
+                user.getMavatarPath()+"/"+user.getMuserName()
+                +I.AVATAR_SUFFIX_JPG));
+        L.e("file="+file.exists());
+        L.e("file="+file.getAbsolutePath());
+        final ProgressDialog pd = new ProgressDialog(mContext);
+        pd.setMessage(getResources().getString(R.string.update_user_avatar));
+        pd.show();
+        NetDao.updateAvatar(mContext, user.getMuserName(), file, new OkHttpUtils.OnCompleteListener<String>() {
+            @Override
+            public void onSuccess(String s) {
+                L.e("s="+s);
+                Result result = ResultUtils.getResultFromJson(s,User.class);
+                L.e("result ="+result);
+                if (result == null){
+                       CommonUtils.showLongToast(R.string.update_usre_avatar_fail);
+                }else {
+                    User u = (User) result.getRetData();
+                    if (result.isRetMsg()){
+                        FuLiCenterApplication.setUser(u);
+                        ImageLoader.setAvatar(ImageLoader.getAvatarUrl(u),mContext,imPersonalDataUserAvatar);
+                        CommonUtils.showLongToast(R.string.update_usre_avatar_success);
+                    }else {
+                        CommonUtils.showLongToast(R.string.update_usre_avatar_fail);
+                    }
+                }
+                pd.dismiss();
+            }
+            @Override
+            public void onError(String error) {
+                CommonUtils.showLongToast(R.string.update_usre_avatar_fail);
+                L.e("error="+error);
+            }
+        });
     }
 
     private void showInfo(){
